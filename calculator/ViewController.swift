4adf36747d0b822f872e3fbad70fa526569b3c1a
Rey Matsunaga <matsunagar97@gmail.com>
2019-01-22 15:27:56 -0800

    first commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,138 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Rey Matsunaga on 1/22/19.
+//  Copyright © 2019 Rey Matsunaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Add background color
+        view.backgroundColor = .black
+        
+        // Create top label for work
+        let calcResult = UILabel()
+        calcResult.text = "0"
+        calcResult.textColor = .white
+        calcResult.textAlignment = NSTextAlignment.right
+        calcResult.font = UIFont(name: "HelveticaNeue-UltraLight",
+                                 size: 70.0)
+        view.addSubview(calcResult)
+        
+        
+        // answer's constraints
+        calcResult.translatesAutoresizingMaskIntoConstraints = false
+        calcResult.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        calcResult.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
+        calcResult.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
+        calcResult.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
+        
+        
+        // Preparing Grid
+        let rowOne = ["AC", "+/-", "%", "/"]
+        let rowTwo = ["7", "8", "9", "*"]
+        let rowThree = ["4", "5", "6", "-"]
+        let rowFour = ["1", "2", "3", "+"]
+        let rowFive = ["0", ".", "="]
+        
+        let rows = [rowOne, rowTwo, rowThree, rowFour, rowFive]
+        
+        // Configure grid
+        let verticalStack = UIStackView()
+        verticalStack.translatesAutoresizingMaskIntoConstraints = false
+        verticalStack.axis = .vertical
+        verticalStack.spacing = 5
+        verticalStack.distribution = .fillEqually
+        
+        var horizontalStackOne: UIStackView
+        var horizontalStackTwo: UIStackView
+        var horizontalStackThree: UIStackView
+        var horizontalStackFour: UIStackView
+        var horizontalStackFive: UIStackView
+        
+        // Configure rows
+        
+        for index in 0...4 {
+            if(index == 0) {
+                horizontalStackOne = UIStackView(arrangedSubviews: createButtons(rows[index]))
+                configureStack(horizontalStackOne)
+                verticalStack.addArrangedSubview(horizontalStackOne)
+            } else if (index == 1) {
+                horizontalStackTwo = UIStackView(arrangedSubviews: createButtons(rows[index]))
+                configureStack(horizontalStackTwo)
+                verticalStack.addArrangedSubview(horizontalStackTwo)
+            }else if (index == 2) {
+                horizontalStackThree = UIStackView(arrangedSubviews: createButtons(rows[index]))
+                configureStack(horizontalStackThree)
+                verticalStack.addArrangedSubview(horizontalStackThree)
+            }else if (index == 3) {
+                horizontalStackFour = UIStackView(arrangedSubviews: createButtons(rows[index]))
+                configureStack(horizontalStackFour)
+                verticalStack.addArrangedSubview(horizontalStackFour)
+            } else {
+                horizontalStackFive = UIStackView(arrangedSubviews: createButtons(rows[index]))
+                configureStack(horizontalStackFive)
+                verticalStack.addArrangedSubview(horizontalStackFive)
+            }
+        }
+        view.addSubview(verticalStack)
+        
+        // Vertical Stack constraints
+        
+        verticalStack.topAnchor.constraint(equalTo: calcResult.topAnchor, constant: 100).isActive = true
+        verticalStack.leadingAnchor.constraint(equalTo: calcResult.leadingAnchor).isActive = true
+        verticalStack.trailingAnchor.constraint(equalTo: calcResult.trailingAnchor).isActive = true
+        verticalStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
+    
+    private func configureStack(_ stack: UIStackView) {
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .horizontal
+        stack.spacing = 5
+        stack.distribution = .fillEqually
+    }
+    
+    private func createButtons(_ named: [String]) -> [UIButton] {
+        
+        return named.map { name in
+            
+            // Initialize basic buttons
+            
+            let button = UIButton()
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.setTitle(name, for: .normal)
+            
+            
+            // switch statement and configure background color and text color (in zero's case, make it default but finally make it wide
+            
+            switch name {
+            case "AC", "+/-", "%":
+                button.backgroundColor = UIColor.gray
+                button.setTitleColor(.white
+                    , for: .normal)
+            case "/", "*", "-", "+", "=":
+                button.backgroundColor = UIColor.orange
+                button.setTitleColor(.white, for: .normal)
+            default:
+                button.backgroundColor = UIColor.darkGray
+                button.setTitleColor(.white, for: .normal)
+            }
+            
+            if(name == "0") {
+                button.widthAnchor.constraint(equalToConstant: )
+            }
+            button.layer.cornerRadius = button.frame.size.width / 2
+            button.clipsToBounds = true
+            return button
+        }
+    
+    }
+
+
+}
+
